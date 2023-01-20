c2bf68adc2275c46f3cfb12f10ffa724d8ad8cb2
hadiljr <hadiljr@gmail.com>
2023-01-20 15:58:35 -0300

    Add Cartridge Rom Header

---
+++ GameBoyEmulator/HardwareComponents/Cartridge/Header/RomHeader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace GameBoyEmulator.HardwareComponents.Cartridge.Header
+{
+    public struct RomHeader
+    {
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+        public byte[] Entry;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 0x30)]
+        public byte[] Logo;
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 16)]
+        public string Title;
+
+        public UInt16 NewLicenseeCode;
+
+        public byte SgbFlag;
+        public byte Type;
+        public byte RomSize;
+        public byte RamSize;
+        public byte DestinationCode;
+        public byte OldLicenseeCode;
+        public byte Version;
+        public byte Checksum;
+        public byte GlobalChecksum;
+    }
+}
